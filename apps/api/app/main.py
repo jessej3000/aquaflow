1ef30ee7992e5e818a7b10bafa7dbd6a31befaf1
jessej3000 <jessej3000@gmail.com>
2026-05-01 11:30:52 +0800

    fix CORS

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Watermaster API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["https://aquaflow-nu-ten.vercel.app","http://localhost:3000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
